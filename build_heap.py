2e6306dcf8c29314326d15dd0c036c562ce2b8f1
kapecNeRTU <janis.snikers@edu.rtu.lv>
2023-05-19 06:16:54 +0000

    1st test

--- build_heap.py
+++ build_heap.py
@@ -3,9 +3,26 @@
 
 def build_heap(data):
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
 
+    def sift_down(i):
+        min_index = i
+        left_child = 2 * i + 1
+        right_child = 2 * i + 2
+
+        if left_child < len(data) and data[left_child] < data[min_index]:
+            min_index = left_child
+
+        if right_child < len(data) and data[right_child] < data[min_index]:
+            min_index = right_child
+
+        if i != min_index:
+            swaps.append((i, min_index))
+            data[i], data[min_index] = data[min_index], data[i]
+            sift_down(min_index)
+
+    n = len(data)
+    for i in range(n // 2 - 1, -1, -1):
+        sift_down(i)
 
     return swaps
 
@@ -15,11 +32,16 @@ def main():
     # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
+    inputChoice = input()
+    if  inputChoice.lower()[0] == "i":
+        n = int(input("input:"))
+        data = list(map(int, input().split()))
 
-
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+    elif inputChoice[0] == "F":
+        #faila nosaukumam tiek pievienots relatīvais path, citādāk neizpildās testi
+        file = open("test/" + input(), mode="r", encoding="utf-8")
+        n = int(file.readlines())
+        data = list(map(int, file.readlines().split()))
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
@@ -30,7 +52,6 @@ def main():
 
     # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
-
 
     # output all swaps
     print(len(swaps))
